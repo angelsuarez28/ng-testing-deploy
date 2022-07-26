1c6ca945c2b9a03aec21a4ce4617ed72cae0f413
asuarezc91 <angelsuarezgis@gmail.com>
2022-07-26 23:15:30 +0200

    feat: google maps modal added

---
+++ src/app/+modules/local-product/local-product.module.ts
@@ -0,0 +1,37 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { RouterModule } from '@angular/router';
+import { LocalProductListComponent } from './components/local-product-list/local-product-list.component'
+import { SharedModule } from 'src/app/shared/shared.module';
+import { LocalProductDetailComponent } from './components/local-product-detail/local-product-detail.component';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+import { PaginatePipe } from 'src/app/pipes/paginate.pipe';
+// import { VisualsModule } from 'src/app/shared/visuals/visuals.module';
+
+//Because is the "local-product" page  ----> "RouterModule.forChild(["   in imports 
+
+@NgModule({
+  declarations: [LocalProductListComponent, LocalProductDetailComponent],
+  imports: [
+    CommonModule,
+    RouterModule.forChild([
+      {
+        path: '',
+        component: LocalProductListComponent
+      },
+      {
+        path: 'detail',
+        component: LocalProductDetailComponent
+      }
+    ]),
+    SharedModule,
+    FormsModule,
+    ReactiveFormsModule
+    // VisualsModule
+  ],
+  exports: [
+    
+  ]
+})
+
+export class LocalProductModule { }
